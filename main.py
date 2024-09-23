39f0610725813c23a315c013273ff66c9c08964f
Sira Drame <sadrame@mail.fhsu.edu>
2024-09-23 05:13:45 -0500

    We added matplotlib and we converted Close costs  to a numpy array and we finally created the graphs as asked.

--- main.py
+++ main.py
@@ -2,17 +2,38 @@
 # Sira Drame
 # Mini Project 1
 import pprint
-
 import yfinance as yf
+from datetime import datetime, timedelta
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+
+# Get today's date
+today = datetime.now()
+
+# Calculate the date 10 days ago
+ten_days_ago = today - timedelta(days=15)
 
 mytickers = ['MSFT','AAPL', 'GOOGL', 'TSLA', 'AMZN']
 
+mytickers.sort()
 for ticker in mytickers:
     result = yf.Ticker(ticker)
-    print(f"Ticket: {ticker}  \tDay High: {result.info['dayHigh']}")
-#msft.info [""]
-
-# get historical market data
-#hist = msft.history(period="10d")
-
-#pprint.pprint(hist)
+    hist = result.history(start=ten_days_ago, end=today)
+    last10days = []
+    for date in hist['Close'][:11]:
+        last10days.append(date)
+    if len(last10days) == 10:
+       maxlist = copy.copy(last10days)
+       maxlist.sort()
+       max_price = maxlist[-1]+10
+       min_price = maxlist[0]-10
+       myarray = np.array(last10days)
+       plt.plot(myarray)
+       plt.xlabel('Days Ago')
+       plt.ylabel('Closing Price')
+       plt.axis((9, 0, min_price, max_price))
+       plt.title(f"{ticker} last 10 closing prices")
+       plt.show()
+    else:
+        print('You  do not have 10 days of data and you only have {len(last10days)} days.')
